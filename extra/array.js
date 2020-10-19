76cf696446166cbfc9442ce565a93253bd747ba6
Aviral <aviral.swarnkar@successive.tech>
2020-10-19 23:10:15 +0530

    feat: update files

--- extra/array.js
+++ extra/array.js
@@ -1,62 +1,120 @@
+// Using map() method
 
-// Using filter method
+const arr3 = [1,2,3,4,5];
+let x = arr3.map(y => y * 10)   //returns an array after implying the function
+console.log("map()", x)          //Complexity = O(n)
+
+//Using reduce() method
+
+const numbers = [175, 50, 25];
+let myFunc = (total, num) => {  //reduces objects to a single value
+    return total - num;
+  }                                //complexity = O(n)
+console.log("reduce()",numbers.reduce(myFunc))
+
+//Using indexOf() method
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+console.log("isindex()", beasts.indexOf('bison'));   // returns index of 1st occurance of the element
+                                      // complexity = O(n)
+
+// Using findIndex() method
+
+const ages = [3, 10, 18, 20];
+let checkAdult = (age) => {         //returns the index of the first element in an array that pass a test
+  return age >= 18;
+};                                     //complexity = O(n)
+console.log("findIndex()", ages.findIndex(checkAdult));
+
+//Using forEach() method
+
+let sum = 0;
+const numbers1 = [65, 44, 12, 4];
+let myFunction = (item) => {            //method calls a function once for each element
+  sum += item;                      //complexity = O(n)
+};
+numbers1.forEach(myFunction);
+console.log("forEach", sum)
+
+//Using concat() method
+
+const array11 = ['a', 'b', 'c']; // used to merge two or more arrays
+const array22 = ['d', 'e', 'f']; //complexity = O(n)
+const array33 = array11.concat(array22);
+console.log("concat", array33);
+
+// Using filter() method
 
 const arr2 = [250,150,100,450,500]
-const result = arr2.filter(arr2 => arr2 > 200)
-console.log(result)
+const result = arr2.filter(arr2 => arr2 > 200)  // creates a new array with all elements that pass thecondition
+console.log("filter()", result)             //Complexity = O(n)
 
 
-// Using from method
+// Using from() method
 
-console.log(Array.from("Aviral"))
-console.log(Array.from([1,2,3,4],x => x*2))
+console.log("from()", Array.from("Aviral")) // returns an Array object from any iterable object.
+console.log("from()", Array.from([1,2,3,4],x => x*2)) //Complexity = O(n)
 
 
-// Using pop method
+// Using pop() method
+
 const name1 = ["Aviral" , "rahul" , " mayank" , "divyansh"]
-name1.pop()
-console.log(name1)
+name1.pop()                             //removes the last element of an array, and returns that element.
+console.log("pop()", name1)             //Complexity = O(1)
 
 
-// Using push method
+// Using push() method
 
 const name = ["Aviral" , "rahul" , " mayank" , "divyansh"]
-name.push("tushar")
-console.log(name)
+name.push("tushar")                 //adds new items to the end of an array, and returns the new length.
+console.log("push()", name)                   //Complexity = O(1)
+
+// Using shift() method
+
+const name5 = ["Aviral" , "rahul" , " mayank" , "divyansh"]
+name5.shift()                             //removes the first element of an array, and returns that element.
+console.log("shift", name5)             //Complexity = O(n)
 
 
-// Using reverse method
+// Using unshift() method
 
-const arr = ["one","two","three"]
-const rev = arr.reverse()
-console.log(rev)
+const name6 = ["Aviral" , "rahul" , " mayank" , "divyansh"]
+name6.unshift("tushar")                 //adds new element to the start of an array, and returns the new length.
+console.log("unshift()", name6)                   //Complexity = O(n)
 
-//Using find method
 
-const array1 = [5, 12, 8, 130, 44];
+// Using reverse() method
+
+const arr = ["one","two","three"]   //method reverses the order of the elements in an array.
+const rev = arr.reverse()              //Complexity = O(n)
+console.log("reverse()", rev)
+
+//Using find() method
+
+const array1 = [5, 12, 8, 130, 44]; //method returns the value of the first element that passes a condition
 const found = array1.find(element => element > 10);
-console.log(found);
+console.log("find()", found);                     //Complexity = O(n)
 
-//Using join method
+//Using join() method
 
 const elements = ['Fire', 'Air', 'Water'];
-console.log(elements.join());
-console.log(elements.join(''));
+console.log("join()", elements.join());   //method returns the array as a string seperated by a seperater
+console.log("join()", elements.join('')); //Complexity = O(n)
 
-//Using slice method
+//Using slice() method
 
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-console.log(animals.slice(2));
-console.log(animals.slice(2, 4));
+console.log("slice()", animals.slice(2)); //returns the selected elements in an array, as a new array object.
+console.log("slice()", animals.slice(2, 4)); //Complexity = O(n)
 
-//Using splice method
+//Using splice() method
 
 const months = ['Jan', 'March', 'April', 'June'];
-months.splice(1, 0, 'Feb');
-console.log(months);
+months.splice(1, 0, 'Feb');  //adds/removes items to/from an array, and returns the removed item(s).
+console.log("splice()", months);        //Complexity = O(n)
 
-// Using sort method
+// Using sort() method
 
-const array2 = [10,11,33,55,77];
-array2.sort(function(a,b){return b-a});
-console.log(array2);
+const array2 = [10,11,33,55,77]; 
+array2.sort(function(a,b){return b-a});     //sorts the items of an array.
+console.log("sort", array2);        //complexity = O(nlog(n))
--- extra/objects.js
+++ extra/objects.js
@@ -1,5 +1,5 @@
 // Initialization of Objects
-let Object1 = {
+let object1 = {
     id: 0,
     name: "user 1",
     designation:["developer", "fresher"],
@@ -7,63 +7,63 @@ let Object1 = {
     hello: function(){
         console.log("Hello", this.name)
     } 
-}
-console.log(Object1.designation);
-Object1.hello();
+};
+console.log(object1.designation);
+object1.hello();
 
-let Object2 = {
+let object2 = {
     id: 1,
     name: "user 2",
     designation: ["developer", "fresher"],
     hello: function(){
         console.log("hello", this.name)
     }
-}
-console.log(Object2.designation);
-Object2.hello();
+};
+console.log(object2.designation);
+object2.hello();
 
 // Using Object.assign method
 
-console.log(Object1);
-let modObject = Object.assign(Object1,Object2); //copies all properties of source into target and returns a modified object
-console.log(modObject);                     
-console.log(Object1);             //original Object also gets modified by assign() method
+console.log(object1);
+let modifiedObject = Object.assign(object1,object2); //copies all properties of source into target and returns a modified object
+console.log(modifiedObject);                     
+console.log(object1);             //original Object also gets modified by assign() method
 
 // Using Object.create method
 
-let modObject1 = Object.create(Object1);
-modObject1.age = 21;
-console.log(modObject1.age);      //create() method returns an object with all the properties of the prototyped object
+let modifiedObject1 = Object.create(object1);
+modifiedObject1.age = 21;
+console.log(modifiedObject1.age);      //create() method returns an object with all the properties of the prototyped object
 
 // Using Object.defineProperties method
 
-Object.defineProperties(Object1,{ pincode : {value: 201301}})  
-console.log(Object1.pincode)        // used to define new or modifies existing properties directly on an object
+Object.defineProperties(object1,{ pincode : {value: 201301}})  
+console.log(object1.pincode)        // used to define new or modifies existing properties directly on an object
 
 //Using Object.entries method
 
-for (const [key,value] of Object.entries(Object1)){
+for (const [key,value] of Object.entries(object1)){
     console.log(key, value)     // used to iterate over an object of key value pairs in the same order they are defined
 }
 
 //Using Object.freeze method
 
-Object.freeze(modObject)       // prevents new modifications on the object
-modObject.name = "temp"       // will throw an error in strict mode
+Object.freeze(modifiedObject)       // prevents new modifications on the object
+modifiedObject.name = "temp"       // will throw an error in strict mode
 
 //Using Object.is method
 
-console.log(Object.is(Object1.name, Object1.age)) // used to compare two properties of an object
+console.log(Object.is(object1.name, object1.age)) // used to compare two properties of an object
 
 //Using Object.keys method
 
-console.log(Object.keys(Object1))   // returns an array of all keys inside the object like a loop would
+console.log(Object.keys(object1))   // returns an array of all keys inside the object like a loop would
 
 //Using Object.values method
 
-console.log(Object.values(Object1)) // returns an array of all the values inside the object
+console.log(Object.values(object1)) // returns an array of all the values inside the object
 
 //Using Object.seal method
 
-Object.seal(modObject1)             // prevents adding new properties but still can add modify previously defined properties
+Object.seal(modifiedObject1)             // prevents adding new properties but still can add modify previously defined properties
 
