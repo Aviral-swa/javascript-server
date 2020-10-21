2266ca90d02b4a9b7aa84b206037f37ab9e0cd09
Aviral <aviral.swarnkar@successive.tech>
2020-10-22 00:54:23 +0530

    feat: update files

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,30 +1,40 @@
 const permissions = {
-    'getUsers': {
-    all: ['head-trainer'],
-    read : ['trainee', 'trainer'],
-    write : ['trainer'],
-    delete: [],
-    },
+    getUsers: {
+        all: ['head-trainer'],
+        read : ['trainee', 'trainer'],
+        write : ['trainer'],
+        delete: []
+},
 
-    'getProduct': {
-    all: ['head-trainer'],
-    read : ['trainee', 'trainer'],
-    write : ['trainer'],
-    delete: ['manager'],
-    }
+    getProduct: {
+        all: ['manager'],
+        read : ['marketingTeam', 'developmentTeam'],
+        write : ['developmentTeam'],
+        delete: []
+}
 }
 
 const hasPermissions = (moduleName, role, permissionType) => {
-    if (permissions[moduleName].all.includes(role)){
-        console.log("true")
-    }
-    else if (permissions[moduleName][permissionType].includes(role)){
-          console.log("true")
+    try{
+        if (permissions[moduleName].all.includes(role)){   //To check if ${role} has 'all' permission and return true 
+                console.log(`${role} has all permissions`)                  //else move to the next condition
+                return true
         }
-    else{
-        console.log("false")
-    }
-    
-    }
-     
-hasPermissions("getUsers", "head-trainer", "read")
+        else if (permissions[moduleName][permissionType].includes(role)){
+                console.log(`${role} has ${permissionType} permissions`)
+                return true
+            }
+        else{
+                console.log(`${role} does not has ${permissionType} permissions`)
+                return false
+            }
+        }
+    catch(err) {
+        console.log("Enter valid moduleName: " + err)
+        }
+}
+ 
+hasPermissions("getProduct", "manager", "all")
+hasPermissions("getRelations", "developmentTeam", "write")
+hasPermissions("getUsers", "head-trainer", "write")
+hasPermissions("getUsers", "trainee", "read")
--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,17 +1,17 @@
 const users = [
     {
-    traineeEmail: 'aviral.swarnkar@successive.tech',
-    reviewerEmail: 'avinash.thube@successive.tech'
+        traineeEmail: 'aviral.swarnkar@successive.tech',
+        reviewerEmail: 'avinash.thube@successive.tech'
     },
     
     {
-    traineeEmail: 'mudit.rajput@live.in' ,
-    reviewerEmail: 'yogesh.singh@successive.tech'
+        traineeEmail: 'mudit.rajput@live.in' ,
+        reviewerEmail: 'yogesh.singh@successive.tech'
     },
     
     {
-    traineeEmail: 'nikhil.rawat@gmail.com',
-    reviewerEmail: 'shalu.sharma@successive.tech'
+        traineeEmail: 'nikhil.rawat@gmail.com',
+        reviewerEmail: 'shalu.sharma@successive.tech'
     }
     
 ];
@@ -23,27 +23,16 @@ const validateEmail = (email) => {
 }
 
 
-const validateUsers = (arr) =>{
-    let validUsers = [];
-    let invalidUsers = [];
-    arr.forEach(function (user) {
-        const {traineeEmail,reviewerEmail} = user; 
-        if (validateEmail(traineeEmail)){
-            validUsers.push(traineeEmail)
+const validateUsers = (user) =>{
+    const validUsers = [];
+    const invalidUsers = [];
+    user.forEach(({traineeEmail, reviewerEmail}) => {
+        validateEmail(traineeEmail) ? validUsers.push(traineeEmail) : invalidUsers.push(traineeEmail)
+        validateEmail(reviewerEmail) ? validUsers.push(reviewerEmail) : invalidUsers.push(reviewerEmail)
         }
-        else{
-            invalidUsers.push(traineeEmail)
-        }
-        if(validateEmail(reviewerEmail)){
-            validUsers.push(reviewerEmail)
-        }
-        else{
-            invalidUsers.push(reviewerEmail)
-        }
-    })
-
-    let countValid = validUsers.length;
-    let countInvalid = invalidUsers.length;
+    )
+    const countValid = validUsers.length;
+    const countInvalid = invalidUsers.length;
     console.log(`There are ${countValid} valid users:`, validUsers)
     console.log(`There are ${countInvalid} invalid users:`, invalidUsers)
     
