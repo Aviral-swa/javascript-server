ed34a8087c200878c26a39a27dd44510c376fd91
Aviral <aviral.swarnkar@successive.tech>
2020-10-27 18:48:49 +0530

    feat: update optimized files

--- src/Server.ts
+++ src/Server.ts
@@ -1,8 +1,8 @@
 import * as express from 'express';
 
 class Server {
-    app;
-    constructor(private config) {
+    private app: any;
+    constructor(private configuration: IConfig) {
         this.app = express();
     }
 
@@ -12,22 +12,21 @@ class Server {
     }
 
     setupRoutes() {
-        const { app } = this;
-        app.get('/health-check', (req, res, next) => {
+        this.app.get('/health-check', (req, res, next) => {
             res.send('I am OK');
         });
         return this;
     }
 
     run() {
-        const { app, config: { port } } = this;
-
-        app.listen(port, (err) => {
+        const { port } = this.configuration;
+        this.app.listen(port, err => {
             if (err) {
-                console.log(err);
+                console.log(`Error: app failed  ${err}`);
             }
             console.log(`app is running on port ${port}`);
         });
+        return this;
     }
 
 }
